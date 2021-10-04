788a14c78468217721d9912839427af02fbe7065
Vladislav Saraev <vladislav.saraev@geropharm.com>
2021-10-04 16:59:47 +0300

    Add CalculatorTests skeleton

--- TextCalculator.Tests/CalculatorTests.cs
+++ TextCalculator.Tests/CalculatorTests.cs
@@ -1,10 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
+using Xunit;
 
 namespace TextCalculator.Tests
 {
-    class CalculatorTests
+    public class CalculatorTests
     {
+        [Theory]
+        [InlineData("1+(2+3)*(4*5)", 101)]
+        public void Calculate_Expression_ReturnTrue(string expression, int result)
+        { 
+            // Arrange
+
+            // Act
+
+            // Assert
+        }
     }
 }
