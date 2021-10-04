08e4e2ba6dafc72fa7399862772b974c2e578409
Vladislav Saraev <vladislav.saraev@geropharm.com>
2021-10-04 17:00:23 +0300

    CalculatorTests InlineData fix

--- TextCalculator.Tests/CalculatorTests.cs
+++ TextCalculator.Tests/CalculatorTests.cs
@@ -8,7 +8,7 @@ namespace TextCalculator.Tests
     public class CalculatorTests
     {
         [Theory]
-        [InlineData("1+(2+3)*(4*5)", 101)]
+        [InlineData("1+(2+3)*(4+5)", 101)]
         public void Calculate_Expression_ReturnTrue(string expression, int result)
         { 
             // Arrange
