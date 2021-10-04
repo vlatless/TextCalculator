e09b0223f57692d3730d5384c6cffca626e555eb
Vladislav Saraev <vladislav.saraev@geropharm.com>
2021-10-04 15:34:43 +0300

    Add project files.

---
+++ TextCalculator.Tests/ExpressionParserTests.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace TextCalculator.Tests
+{
+    public class ExpressionParserTests
+    {
+        [Theory]
+        [InlineData("1+1", new double[] { 1d, 1d })]
+        [InlineData("1-1", new double[] { 1d, 1d })]
+        [InlineData("1*1-1.1", new double[] { 1d, 1d, 1.1d })]
+        [InlineData("1*1+1.111", new double[] { 1d, 1d, 1.111d })]
+        public void Expression_Operation_ReturnsOperands(string input, double[] expectedOperands)
+        {
+            // Arrange 
+            var parser = new ExpressionParser();
+
+            // Act 
+            var actualOperands = parser.FindOperands(input).ToArray();
+
+            // Assert
+            Assert.Equal(expectedOperands, actualOperands);
+        }
+    }
+}
---
+++ TextCalculator/ExpressionParser.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Text.RegularExpressions;
+
+namespace TextCalculator
+{
+    public class ExpressionParser
+    {
+        private readonly string allOperandsPattern = @"\d+(\.\d+)?";
+        private readonly string expressionsBetweenExternalBracketsPattern = @"(\(.+\))";
+        private readonly string expressionsBetweenBracketsInsideExternalBracketsPattern = @"\(([^()]*)\)";
+
+        private MatchCollection ReturnMatchCollection(string pattern, string expression) => new Regex(pattern).Matches(expression);
+
+        public void ParseExpression(string expression)
+        {
+
+        }
+
+        private string FindAllExpressionsInsideExternalBrackets(string expression)
+        {
+            var matches = ReturnMatchCollection(expressionsBetweenExternalBracketsPattern, expression);
+
+            foreach(Match match in matches)
+            {
+                FindAllExpressionsInsideExternalBrackets(match.Value);
+            }
+            return "hah";
+        }
+
+        private void FindExpressionsBetweenBrackets(string expression)
+        {
+            var matches = ReturnMatchCollection(expressionsBetweenBracketsInsideExternalBracketsPattern, expression);
+        }
+
+        public List<double> FindOperands(string expression)
+        {
+            var matches = ReturnMatchCollection(allOperandsPattern, expression);
+            var operandsList = new List<double>();
+
+            foreach (Match match in matches)
+            {
+                operandsList.Add(double.Parse(match.Value));
+            }
+
+            return operandsList;
+        }
+    }
+}
