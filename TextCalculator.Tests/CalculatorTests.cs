9486299fecfaf27f3924aeb8e29cf9758341deee
Vladislav Saraev <vladislav.saraev@geropharm.com>
2021-10-05 09:32:06 +0300

    Add falt test

--- TextCalculator.Tests/CalculatorTests.cs
+++ TextCalculator.Tests/CalculatorTests.cs
@@ -26,31 +26,82 @@ namespace TextCalculator.Tests
         [Theory]
         [InlineData("1+2+3", 6d)]
         public void Calculate_ExpressionWithNoBrackets_ReturnTrue(string expression, double expectedResult)
-        { 
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var actualResult = calculator.CalculateExpression(expression);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
         }
 
         [Theory]
         [InlineData("1+(2-3)", 0d)]
         public void Calculate_ExpressionWithBrackets_ReturnTrue(string expression, double expectedResult)
         {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var actualResult = calculator.CalculateExpression(expression);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
         }
 
         [Theory]
         [InlineData("1+2+3+4", 10d)]
         public void Calculate_ExpressionWithOnlySum_ReturnTrue(string expression, double expectedResult)
         {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var actualResult = calculator.CalculateExpression(expression);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
         }
 
         [Theory]
         [InlineData("1-2-3-4", -10d)]
         public void Calculate_ExpressionWithOnlySub_ReturnTrue(string expression, double expectedResult)
         {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var actualResult = calculator.CalculateExpression(expression);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
         }
 
         [Theory]
         [InlineData("((3-1) + (2-1)*(4-1))", 5d)]
         public void Calculate_ExpressionWithOnlyBrackets_ReturnTrue(string expression, double expectedResult)
         {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act
+            var actualResult = calculator.CalculateExpression(expression);
+
+            // Assert
+            Assert.Equal(expectedResult, actualResult);
+        }
+
+        [Theory]
+        [InlineData("0/1")]
+        public void Calculate_ExpressionSubWithZero_ReturnFault(string expression)
+        {
+            // Arrange
+            var calculator = new Calculator();
+
+            // Act & Assert
+            Assert.Throws<Exception>(()=> calculator.CalculateExpression(expression));
         }
     }
 }
